18be3f3214ee3dee9ad7bf2541ff209bd568b4f8
58Harshil <58harshilsirvichoudhary@gmail.com>
2024-08-14 19:16:47 +0530

    conversion of data types

---
+++ six.js
@@ -0,0 +1,17 @@
+let score = "Harshil";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let valueInNumber = Number(score);
+console.log(typeof(valueInNumber));
+console.log(valueInNumber); 
+
+let isLoggedIn = "";
+let booleanIsLoggedIn = Boolean(isLoggedIn);
+console.log(booleanIsLoggedIn);
+
+let someNumber = 66;
+let stringNumber = String(someNumber);
+console.log(stringNumber);
+console.log(typeof(someNumber));
