b5bf7cb354aa43063448c8f82a8d024411b08351
58Harshil <58harshilsirvichoudhary@gmail.com>
2024-08-14 18:53:39 +0530

    dataTypes

---
+++ five.js
@@ -0,0 +1,17 @@
+"use strict"; // treat all JS code as newer versions
+
+//alert("Hello"); //we a re using node.js not browser
+
+console.log(3 + 3);
+console.log("Harshil");
+
+let name = "Harshil";   
+let age = 20;
+let isLoggedIn = false; 
+let state = null; 
+
+console.table([name, age, isLoggedIn, state]);
+
+console.log(typeof undefined);
+console.log(typeof null);
+
---
+++ four.js
@@ -0,0 +1,15 @@
+const accountId = 14445;
+let accountEmail = "58harshilsirvichoudhary@Gmail.com";
+var accountPassword = "2344";
+accountCity = "Surat";
+
+//accountId = 2; //not allowed
+
+accountEmail = "youharshil@gmail.com";
+accountPassword = "600"
+accountCity = "Vapi";
+
+//console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity]);
+
