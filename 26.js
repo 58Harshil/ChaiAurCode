6c6c42499012830c0f2a9fb87e0ac5bd2056e7cb
58Harshil <58harshilsirvichoudhary@gmail.com>
2024-08-17 00:03:38 +0530

    control flow

---
+++ 26.js
@@ -0,0 +1,90 @@
+// if
+//const isUserLoggedIn = true;
+//const temperatures = 41;
+/*
+if (temperatures === 40) {
+  console.log("less than 50");
+} else {
+  console.log("temperatures is greater than 50");
+}
+*/
+/*
+const score = 200
+if(score > 100){
+   var power = "Fly"
+    
+}
+console.log(`User power : ${power}`);
+*/
+/*
+const balance = 1000;
+//if(balance > 500) console.log("Test");
+
+if (balance < 500) {
+  console.log("Less than 500");
+} else if (balance < 750) {
+  console.log("Less than 750");
+} else if (balance < 900) {
+  console.log("Less than 900");
+} else {
+  console.log("less than 1200");
+}
+*/
+/*
+const userLoggedIn = true;
+const debitCard = true;
+const loggedInFromGoogle = false;
+const loggedInFromEmail = true;
+if (userLoggedIn && debitCard && 2 == 3) {
+  console.log("aloow to buy course");
+}
+if (loggedInFromGoogle || loggedInFromEmail) {
+  console.log("User logged in");
+}
+*/
+/*
+const month = 2
+switch (month){
+  case 1:
+    console.log("January");
+    break;
+  case 2:
+    console.log("February");
+    break;
+  case 3:
+    console.log("March");
+    break;
+  case 4:
+    console.log("April");
+    break;
+
+  default:
+    console.log("Default case match");
+    
+    break;
+}
+    */
+/*
+const userEmail = "user@example.com";
+if(userEmail){
+  console.log("Got user email");
+  
+}else{
+  console.log("Don't have user email");
+  
+}
+  */
+
+let val1;
+//val1 = 5 ?? 10
+//val1 = null ?? 10
+//val1 = undefined ?? 15
+val1 = null ?? 10 ?? 15
+//console.log(val1);
+
+
+//condition ? true : false;
+
+const iceTeaPrice = 100
+iceTeaPrice <=80 ? console.log("Less than 80") :console.log("More than 80");
+
