1a732aa3a76adb09bbf35045d269d3b120cee7ff
58Harshil <58harshilsirvichoudhary@gmail.com>
2024-08-17 01:19:39 +0530

    loops

---
+++ 27.js
@@ -0,0 +1,52 @@
+// for
+
+for(let i = 0; i < 10; i++){
+    const element = i;
+    if(element == 5){
+    //    console.log("5 is best number");
+        
+    }
+   // console.log(element);
+    
+}
+
+for (let i = 1; i <= 10; i++){
+   // console.log(`Outer loop value: ${i}`);
+    
+    for (let j = 1; j <= 10; j++){
+   // console.log(`inner loop value: ${j} and inner loop${i}`);
+   // console.log(i + '*' + j + ' = ' + i*j);
+    
+}
+
+}
+
+let arr = ["Flash", "Batman","Superhero",];
+//console.log(arr.length);
+
+for (let i = 0; i < arr.length; i++) {
+    const element = arr[i];
+   // console.log(element);
+    
+}
+
+for (let i = 1; i <= 20; i++) {
+    //const element = array[i];
+    if(i==5){
+        break;
+    }
+   // console.log(i);
+    
+    
+}
+
+
+for (let i = 1; i <= 20; i++) {
+    //const element = array[i];
+    if(i==5){
+        continue;
+    }
+   console.log(i);
+    
+    
+}
---
+++ 28.js
@@ -0,0 +1,20 @@
+let i = 1;
+while(i<=20){
+//console.log(`Value of index is ${i}`);
+i = i + 2;
+
+}
+
+let arr = ["Flash", "Batman","Superhero",];
+let ar = 0;
+while(ar<arr.length){
+//console.log(arr[ar]);
+ar++;
+}
+
+
+let score = 11;
+do{
+console.log(`score: ${score}`);
+score++
+}while(score<=10)
